7897a49bbaffa860f359e4005b60fe3e68d013c2
Niraviman <niravimansingharoy71@gmail.com>
2023-11-13 11:07:36 +0600

    Unit Testing

---
+++ app/src/test/java/com/example/unittesting/utils/HelperTest.kt
@@ -0,0 +1,29 @@
+package com.example.unittesting.utils
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class HelperTest {
+
+    @Test
+    fun isPallindrome() {
+        //Arrange
+        val helper = Helper()
+        //Act
+        val result = helper.isPallindrome("hello")
+        //Assert
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun isPallindrome_inputString_level_expectedTrue() {
+        //Arrange
+        val helper = Helper()
+        //Act
+        val result = helper.isPallindrome("level")
+        //Assert
+        assertEquals(true, result)
+    }
+
+}
